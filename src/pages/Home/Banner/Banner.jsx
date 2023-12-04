6c9c6df5dd13084fb4724d23c76e1218ff4859cb
mostafizur rahman <dev.mostafizurrahman@gmail.com>
2023-12-04 08:05:09 +0600

    banner part added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,79 +1,34 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
-import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import portBg from "../../../assets/portBg.png"
 import "./Banner.css";
 import Container from "../../../components/Container";
-import { Link } from "react-router-dom";
+
 
 
 const Banner = () => {
 
   return (
-    <div id="banner-part" className="banner-part primaryBg px-2 py-[120px]">
+    <div id="banner-part" >
       <Container>
-        <div className="flex flex-col sm:flex-row justify-between">
-          <div>
-            <h3 className=" primaryFont lg:text-4xl md:text-3xl sm:text-2xl font-bold">
-              {" "}
-              <span className="text-[#00b4d8]">Hello,</span>{" "}
-              <span className="text-[#00b4d8]">This is...</span>
-            </h3>
-            <h1 className="text-6xl  font-bold text-[#00b4d8] my-4">
-              Mostafizur Rahman
-            </h1>
-            <p className="lg:text-4xl md:text-4xl sm:text-3xl font-bold text-[#00b4d8]  flex mt-2">
-              I'm a{" "}
-              <span className="ms-3 text-white">
-                <Typewriter
-                  options={{
-                    strings: [
-                      " Front-end Developer",
-                      " React Developer",
-                      " Mern Stack Developer",
-                      " Web Designer",
-                    ],
-                    autoStart: true,
-                    loop: true,
-                  }}
-                />
-              </span>
-            </p>
+        <div className="banner-container grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-3">
+          <div className="banner-left h-[500px]">
+            <h3 className="lg:text-3xl md:text-2xl sm:text-1xl primaryFont font-bold text-[#00b4d8]">Hello, This is ..</h3>
+            <h1 className="lg:text-6xl md:text-3xl sm:text-2xl primaryFont font-bold text-[#00b4d8] my-4">Mostafizur Rahman</h1>
+            <h2 className="lg:text-2xl md:text-2xl sm:text-1xl primaryFont font-normal text-white">Front-end Developer</h2>
 
+            <p className="primaryFont sm:text-xl text-white mt-3">A passionate Front-end Developer 🚀 <br />
+               having an experience of building Web applications with Tailwind/ JavaScript/ React Js/ Next JS/ Express / MongoDB  and some other cool libraries and frameworks database and tools.</p>
 
-            
-
-            
-
-            <div className="w-[130px]  mt-4">
-              <div className="flex justify-between">
-                <div className="item w-[50px] h-[50px] secondaryBg hover:bg-white cursor-pointer transition-all rounded-full flex justify-center items-center">
-                  <Link to="https://github.com/md-mostafiz181" target="_blank">
-                    <FaGithub className="text-4xl text-[#00b4d8]" />
-                  </Link>
-                </div>
-                <div className="item w-[50px] h-[50px] secondaryBg hover:bg-white cursor-pointer transition-all rounded-full flex justify-center items-center">
-                  <Link
-                    to="https://www.linkedin.com/in/mostafiz181/"
-                    target="_blank"
-                  >
-                    <FaLinkedinIn className="text-4xl text-[#00b4d8]" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-
-
-
-            <a className="btn-cv text-white  mb-5" href="Frontend developer resume of Mostafiz.pdf" download="Frontend developer resume of Mostafiz.pdf">Download Resume</a>
-
-            
+              <a className="btn-resume primaryFont" href="Frontend developer resume of Mostafiz.pdf" download="Frontend developer resume of Mostafiz.pdf">Download Resume</a>
           </div>
+          <div className="banner-right flex justify-center items-center h-[500px] ">
+            <div className="img-box">
+              <img src={portBg} alt="" />
 
-          <div className="box w-[350px] h-[350px] secondaryBg rounded-full">
-            <img className="img" src={portBg} alt="" />
+            </div>
           </div>
         </div>
+
       </Container>
     </div>
   );
